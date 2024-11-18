ead970054af7f80a81d5f648e641c15b9f3a007e
giboin <louisg@bam.tech>
2024-11-18 21:33:40 +0100

    doc: shader mask

--- lib/shader_mask/view/shader_mask_page.dart
+++ lib/shader_mask/view/shader_mask_page.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_shaders/flutter_shaders.dart';
 
+/// How can we apply a shader to a widget in Flutter?
+/// The `ShaderMask` widget is the answer.
+///
+/// It allows you to apply a shader to a widget, using a `ShaderCallback`.
+/// In the `ShaderCallback`, you can use the same logic as in the
+/// `paint` method of a custom painter to pass values to the shader.
+///
+/// The shader will then be applied to the child of the `ShaderMask`.
 class ShaderMaskPage extends StatelessWidget {
   const ShaderMaskPage({super.key});
 
