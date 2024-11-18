5fe2c7640e3f49fb7abcb0104fc66e3178d09f5d
giboin <louisg@bam.tech>
2024-11-18 21:30:45 +0100

    doc: shader builder page

--- lib/shader_builder/view/shader_builder_page.dart
+++ lib/shader_builder/view/shader_builder_page.dart
@@ -2,6 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_shaders/flutter_shaders.dart';
 import 'package:glow_stuff_with_flutter/gradient/view/gradient_page.dart';
 
+/// The most straightforward way of using shaders in Flutter.
+/// This example shows how to use a .frag file with a `ShaderBuilder`.
+///
+/// `ShaderBuilder` is a widget from the `flutter_shaders` package.
+/// It allows you to create a shader from a .frag file and pass it to a child
+/// widget, without having to initialize the shader in the bootstrap method.
+///
+/// Then, we can use the shader in the same custom painter as before.
+///
+/// The output is the same as the previous example, but the shader is now
+/// initialized in the page itself.
 class ShaderBuilderPage extends StatelessWidget {
   const ShaderBuilderPage({super.key});
 
